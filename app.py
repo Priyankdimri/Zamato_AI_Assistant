5eaacba80aa16475e160b777cf8ed7dcde11f685
Priyankdimri <114757150+Priyankdimri@users.noreply.github.com>
2024-06-25 20:30:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import chainlit as cl
+from src.llm import ask_order,messages
 
 @cl.on_message
 async def main(message:cl.Message):
-    
-    
-    
+    message.append({"role":"system","content":message.content})
+    response=ask_order(messages)
+    messages.append({"role":"assistant","content":response})
     await cl.Message(
-        content=f"Received:{message.content}",
+        content=response,
     ).send()
