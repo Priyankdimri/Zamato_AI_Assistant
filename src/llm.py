e8bcf58cfc1ab307b583f5b17cab928c0e43bd36
Priyankdimri <114757150+Priyankdimri@users.noreply.github.com>
2024-06-25 13:18:37 +0530

    Add files via upload

--- src/llm.py
+++ src/llm.py
@@ -1,1 +1,8 @@
-
+from openai import OpenAI
+client=OpenAI()
+def ask_order(message,model="GPT-3.5-Turbo",temprature=0):
+    response=client.chat.completions.create(
+        model=model,
+        temprature=temprature,
+        message=messsage
+    )
